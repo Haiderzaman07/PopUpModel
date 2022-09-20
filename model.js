c648d9cd2429ab98714e3c76f2fc2963d1c449b1
haider <haider18857@gmail.com>
2022-09-20 06:45:30 +0500

    Initial Commit

---
+++ model.js
@@ -0,0 +1,6 @@
+document.getElementById("open-model").addEventListener("click",function(){
+    document.getElementById("overly").style.display="block"
+})
+document.getElementById("close-model").addEventListener("click",function(){
+    document.getElementById("overly").style.display="none"
+})
